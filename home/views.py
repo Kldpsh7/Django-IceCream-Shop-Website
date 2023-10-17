6599458945987ffa1a26e4a4393321813602299a
Kuldeep Sahrma <kldpsh77@gmail.com>
2023-10-17 18:31:54 +0530

    Saving Data to Model

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render,HttpResponse
+from datetime import datetime
+from home.models import Contact
 
 # Create your views here.
 def index(request):
@@ -18,4 +20,12 @@ def services(request):
 
 def contact(request):
     #return HttpResponse('<h1>This is contact page</h1>')
+    if request.method == 'POST':
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        
+        contact=Contact(name=name,email=email,phone=phone,date=datetime.today())
+        contact.save()
+
     return render(request,'contact.html')
